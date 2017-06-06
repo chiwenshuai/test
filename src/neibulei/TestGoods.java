39498c33eb8afc34ae6f415e46df82994a98faa5
chiwenshuai <604202911@qq.com>
2017-06-06 21:20:03 +0800

    123123

--- src/neibulei/TestGoods.java
+++ src/neibulei/TestGoods.java
@@ -4,7 +4,6 @@ public class TestGoods {
 	public static void main(String[] args) {  
         Goods p = new Goods();  
         Contents c = p.cont();  
-        System.out.println();
         Destination d = p.dest("Beijing");  
     }  
 }
